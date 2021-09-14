0104558cf9d1dbe270aaa8890cbd233c1593c689
Prateek <friskycodeur@gmail.com>
2021-09-14 15:58:28 +0530

    learning: DAY 2

--- budget-app/src/components/ExpenseItem.js
+++ budget-app/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(data) {
-  const month = data.date.toLocaleString("en-US", { month: "long" });
-  const day = data.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = data.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month} </div>
-        <div>{day}</div>
-        <div>{year} </div>
-      </div>
+      <ExpenseDate date={data.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{data.title}</h2>
       </div>
---
+++ budget-app/src/components/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from "./ExpenseItem";
+function Expenses() {
+  const expenses = [
+    { title: "Swiggy Instamart", amount: 150, date: new Date(2021, 8, 6) },
+    { title: "Burger", amount: 50, date: new Date(2021, 8, 6) },
+    { title: "Wifi", amount: 200, date: new Date(2021, 8, 9) },
+  ];
+
+  return (
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
