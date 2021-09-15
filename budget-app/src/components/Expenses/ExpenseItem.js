02829ba7b2d90d1277d5d3fc043801e05c66f767
Prateek <friskycodeur@gmail.com>
2021-09-15 22:25:06 +0530

    Day 3 learning

--- budget-app/src/components/Expenses/ExpenseItem.js
+++ budget-app/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(data) {
+  const [title, setTitle] = useState(data.title);
+
+  function changeTitleHandler() {
+    setTitle("Updated!");
+    console.log(title);
+  }
   return (
     <div className="expense-item">
       <ExpenseDate date={data.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{data.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">₹ {data.amount}</div>
+      <button onClick={changeTitleHandler} className="expense-item__btn">
+        Change Title
+      </button>
     </div>
   );
 }
